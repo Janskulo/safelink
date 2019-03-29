6efc887343c211b19a83db0095dfb6a22213ce07
Janskulo <47676321+Janskulo@users.noreply.github.com>
2019-03-29 22:09:19 +0700

    Update blanter-newsticker.js

--- blanter-newsticker.js
+++ blanter-newsticker.js
@@ -50,7 +50,7 @@ function recent_post_style(){
     var s = "<style type='text/css'>";
     s += "#recent_post{";
     s += "margin:0px;";
-    s += "width:auto;";
+    s += "width:85%;";
     s += "background:#fff;";
     s += "}";
     s += "</style>";
