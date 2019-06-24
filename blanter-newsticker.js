6961b9c01123c6d8acc54798472ac2247d99f3b8
Janskulo <47676321+Janskulo@users.noreply.github.com>
2019-06-24 07:13:13 +0700

    Update blanter-newsticker.js

--- blanter-newsticker.js
+++ blanter-newsticker.js
@@ -62,7 +62,7 @@ function recent_post_content(){
     if(info_text){
     s += "<div class='wrapper'>";
     s += "<div class='newstitle'>";
-    s += "<a href='http://thejanskulo.blogspot.com/search/label/Download%20Mp3?max-results=10'>MP3 Baru =></a>";
+    s += "<a href='http://thejanskulo.blogspot.com/search/label/Download%20Mp3?max-results=15'>MP3 Baru =></a>";
     s += "</div>";
     }
     s += "  <marquee style='float:left; width:82%' scrollAmount='"+scrolling_speed+"'>";
